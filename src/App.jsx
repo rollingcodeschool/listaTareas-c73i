00217545efd01f25f67438e93a60a1fab7237a1c
Emilse Arias <emilsearias@gmail.com>
2023-12-13 11:22:38 -0300

    agregamos el maquetado del componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Container className="my-4">
+        <h1 className="text-center">Lista de tareas</h1>
+      </Container>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        // todo: bajar el footer
+        <footer className="bg-dark text-light text-center py-3">
+            <p>&copy; Todos los derechos reservados</p>
+        </footer>
+    );
+};
+
+export default Footer;
