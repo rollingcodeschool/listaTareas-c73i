c3fd812cb0364a5d1946094d7741c7258eefed80
Emilse Arias <emilsearias@gmail.com>
2023-12-13 22:11:43 -0300

    agregamos el código del componente FormularioTarea

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Footer from "./components/Footer";
+import FormularioTarea from "./components/FormularioTarea";
 
 function App() {
   return (
     <>
       <Container className="my-4">
         <h1 className="text-center">Lista de tareas</h1>
+        <FormularioTarea></FormularioTarea>
       </Container>
       <Footer></Footer>
     </>
